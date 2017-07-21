3841b9aa5e7efed40fe75f71a2182661e484d5cd
NightTinker <travel_special@hotmail.com>
2017-07-22 05:44:00 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HolderComponent } from './holder/holder.component';
+import { SeaListComponent } from './sea-list/sea-list.component';
+import { VideosListComponent } from './videos-list/videos-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HolderComponent,
+    SeaListComponent,
+    VideosListComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sea-list/sea-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sea-list',
+  templateUrl: './sea-list.component.html',
+  styleUrls: ['./sea-list.component.css']
+})
+export class SeaListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

62127cac81126e0306b99fd1b912ce070851b038
NightTinker <travel_special@hotmail.com>
2017-07-27 04:11:09 +0700

    send data to seas-list:/id - no directive mouse over

--- src/app/home/sea-detail/sea-detail.component.ts
+++ src/app/home/sea-detail/sea-detail.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import {Seas} from '../seas.model';
-import {SeaPhotoService} from '../sea-photo.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+
+import {Sea,SeaPhotoService} from '../sea-photo.service';
+import {slideInDownAnimation} from '../../animations';
+
 
 @Component({
   selector: 'app-sea-detail',
   templateUrl: './sea-detail.component.html',
-  styleUrls: ['./sea-detail.component.css']
+  styleUrls: ['./sea-detail.component.css'],
+  animations: [ slideInDownAnimation ]
 })
 export class SeaDetailComponent implements OnInit {
-  seas: Seas;
-  id: number;
 
-  constructor(private seaPhotoService: SeaPhotoService,
-              private route: ActivatedRoute) { }
+  // @HostBinding('@routeAnimation') routeAnimation = true;
+  // @HostBinding('style.display')   display = 'block';
+  // @HostBinding('style.position')  position = 'absolute';
+
+  sea: Sea;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: SeaPhotoService
+  ) {}
 
   ngOnInit() {
-
+    this.route.paramMap
+      .switchMap((params: ParamMap) =>
+        this.service.getSea(params.get('id')))
+      .subscribe((sea: Sea) => this.sea = sea);
   }
+
+  // gotoSeas() {
+  //   let seaId = this.sea ? this.sea.id : null;
+  //   // Pass along the hero id if available
+  //   // so that the HeroList component can select that hero.
+  //   // Include a junk 'foo' property for fun.
+  //   this.router.navigate(['/test', { id: seaId, foo: 'foo' }]);
+  // }
 
 }
